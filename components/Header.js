17a778df9fff3a0bd74f9f4c637e8742b6392128
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-12-23 08:51:58 +0100

    Mobile nav

--- components/Header.js
+++ components/Header.js
@@ -7,10 +7,10 @@ export default function Header() {
   const [visible, setVisible] = useState(false);
 
   return (
-    <header className="py-6 shadow">
+    <header className={`py-6 ${!visible && "shadow"}`}>
       <div className="max-w-screen-xl mx-auto px-8 flex items-center justify-between">
         <span className="text-2xl font-bold text-slate-900">iPhone 13 Pro</span>
-        <nav className={`${visible ? "block" : "hidden"} sm:block`}>
+        <nav className={`${visible ? "flex" : "hidden"} flex-col sm:flex-row items-center sm:justify-end sm:flex absolute sm:static z-10 bg-white top-24 left-0 right-0 pb-8 sm:pb-0 border-b border-slate-300 sm:border-none`}>
           {[
             { text: "Domů", href: "/" },
             { text: "O produktu", href: "/oproduktu" },
@@ -19,12 +19,12 @@ export default function Header() {
           ].map((link, i) => {
             return (
               <Link href={link.href} key={i}>
-                <a className="text-lg font-medium text-slate-600 transition duration-200 hover:text-slate-900 ml-4">{link.text}</a>
+                <a className="text-lg font-medium text-slate-600 transition duration-200 hover:text-slate-900 mb-2 sm:mb-0 sm:ml-4">{link.text}</a>
               </Link>
             );
           })}
         </nav>
-        <div onClick={() => setVisible(!visible)} className="cursor-pointer flex items-center justify-center">
+        <div onClick={() => setVisible(!visible)} className="cursor-pointer flex items-center justify-center sm:hidden">
           <FontAwesomeIcon icon={faBars} className="w-8 h-8 text-3xl text-slate-700" />
         </div>
       </div>
